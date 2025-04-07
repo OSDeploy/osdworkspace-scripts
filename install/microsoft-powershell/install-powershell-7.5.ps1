59db5d9d3311920300b7c65ca974640bb4774b01
David Segura - PowerShell MVP <david@segura.org>
2025-04-06 22:06:58 -0500

    Update install-powershell-7.5.ps1

--- install/microsoft-powershell/install-powershell-7.5.ps1
+++ install/microsoft-powershell/install-powershell-7.5.ps1
@@ -1,2 +1,2 @@
 # Install PowerShell 7.5
-winget install -e --id Microsoft.PowerShell --scope user --override '/Passive ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ADD_PATH=1'
+winget install -e --id Microsoft.PowerShell --scope machine --override '/Passive ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ADD_PATH=1'
